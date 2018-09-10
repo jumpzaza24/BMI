30839970a212bac1df579ae6ccdb8a3058677594
jumpzaza24 <43144089+jumpzaza24@users.noreply.github.com>
2018-09-11 00:05:31 +0700

    Update BMI.java

--- BMI.java
+++ BMI.java
@@ -3,9 +3,9 @@ class BMI{
     public static void main(String[] args) {
       Scanner sc = new Scanner (System.in);
       System.out.print("Enter Weight in KG => ");
-      double w = sc.nextInt();
+      double w = sc.nextDouble();
       System.out.print("Enter Hight in CM => ");
-      double h = sc.nextInt();
+      double h = sc.nextDouble();
 	  double ht = h/100.00 ;
 	  double sum = w/(ht*ht) ;
       System.out.print("BMI for your => "+sum);  
